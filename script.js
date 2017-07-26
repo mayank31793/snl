ce20977a05c65464dedd723afc2d16d77b3b4cc7
mayank <mayank31793@gmail.com>
2017-07-27 00:05:58 +0530

    3rd commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ setTimeout(function(){document.getElementById('playIn').play();},1000);
 var buttonValue;
 var namePlayer1;
 var namePlayer2;
+var clickDice = 0;
 
 function gridSelect(val){
 	buttonValue = val.value;
@@ -11,7 +12,8 @@ function gridSelect(val){
 
 function hideDiv(){
 	if(buttonValue==undefined || document.getElementById('p1').value==""
- 		|| document.getElementById('p2').value==""
+ 		|| document.getElementById('p2').value=="" || 
+ 		document.getElementById('p1').value==document.getElementById('p2').value
 	){
 		window.location.reload();
 	}
@@ -24,9 +26,77 @@ function hideDiv(){
 	document.getElementById('gameId').classList.remove("gameClass1");
 	console.log('button value is '+buttonValue);
 	document.getElementById('playOut').play();
-	for(var i=0;i<buttonValue;i++){
-		var elem = document.createElement('div');
-		elem.className = "elemStyle";
-		document.getElementById('gameId').appendChild(elem);
+	for(var i=0;i<buttonValue;i++){	
+
+		if(buttonValue==36){
+			if((i+1)%6==0){
+				var elem = document.createElement('div');
+				var elemBr = document.createElement('br');
+				var text = document.createTextNode(i+1);
+				elem.appendChild(text);
+				elem.className = "elemStyle";
+				document.getElementById('gameId').appendChild(elem);
+				document.getElementById('gameId').appendChild(elemBr);
+			}
+			else{
+				var elem = document.createElement('div');
+				var text = document.createTextNode(i+1);
+				elem.appendChild(text);
+				elem.className = "elemStyle";
+				document.getElementById('gameId').appendChild(elem);
+			}
+		}
+
+		if(buttonValue==64){
+			if((i+1)%8==0){
+				var elem = document.createElement('div');
+				var elemBr = document.createElement('br');
+				var text = document.createTextNode(i+1);
+				elem.appendChild(text);
+				elem.className = "elemStyle";
+				document.getElementById('gameId').appendChild(elem);
+				document.getElementById('gameId').appendChild(elemBr);
+			}
+			else{
+				var elem = document.createElement('div');
+				var text = document.createTextNode(i+1);
+				elem.appendChild(text);				
+				elem.className = "elemStyle";
+				document.getElementById('gameId').appendChild(elem);
+			}
+		}
+
+		if(buttonValue==100){
+			if((i+1)%10==0){
+				var elem = document.createElement('div');
+				var elemBr = document.createElement('br');
+				var text = document.createTextNode(i+1);
+				elem.appendChild(text);
+				elem.className = "elemStyle";
+				document.getElementById('gameId').appendChild(elem);
+				document.getElementById('gameId').appendChild(elemBr);
+			}
+			else{
+				var elem = document.createElement('div');
+				var text = document.createTextNode(i+1);
+				elem.appendChild(text);
+				elem.className = "elemStyle";
+				document.getElementById('gameId').appendChild(elem);
+			}
+		}		
+
 	}
+}
+
+function DiceRoll(){
+	clickDice+=1;
+	console.log(clickDice);
+	if(clickDice%2==0){
+		document.getElementById('playerTurn').innerHTML = "Turn: "+document.getElementById('p1').value;
+	}
+	else{
+		document.getElementById('playerTurn').innerHTML = "Turn: "+document.getElementById('p2').value;
+	}
+	var random = Math.floor((Math.random() * 6) + 1);
+	document.getElementById('diceRollValue').innerHTML = random;
 }
