a0125a842a8a63e54b1fcfb0561d49fda7bbd4c1
mayank <mayank31793@gmail.com>
2017-07-26 20:03:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+setTimeout(function(){document.getElementById('playIn').play();},1000);
+
+var buttonValue;
+var namePlayer1;
+var namePlayer2;
+
+function gridSelect(val){
+	buttonValue = val.value;
+	console.log('val '+buttonValue);
+}
+
+function hideDiv(){
+	if(buttonValue==undefined || document.getElementById('p1').value==""
+ 		|| document.getElementById('p2').value==""
+	){
+		window.location.reload();
+	}
+	namePlayer1 = document.getElementById('p1').value;
+	console.log(namePlayer1);
+	namePlayer2 = document.getElementById('p2').value;
+	console.log(namePlayer2);
+	document.getElementById('detailsId').style.display = "none";
+	console.log("hide");
+	document.getElementById('gameId').classList.remove("gameClass1");
+	console.log('button value is '+buttonValue);
+	for(var i=0;i<buttonValue;i++){
+		var elem = document.createElement('div');
+		elem.className = "elemStyle";
+		document.getElementById('gameId').appendChild(elem);
+	}
+}
